3cf771e15bc5cf505a071ea9bd5e3578e2983d14
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2021-01-02 16:17:10 -0500

    refactor index.js

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-require('dotenv').config();
+const express = require('express');
+const shortid = require('shortid');
 
-const server = require('./api/server.js');
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.json({ potluck: 'Planner' });
+});
+
+server.get('/potluck', (req, res) => {
+  res.json({ potluck: 'Event Planner' });
+});
 
 const PORT = process.env.PORT || 4500;
 
 server.listen(PORT, () => {
   console.log(`n*** Server is listening on  port ${PORT} ***\n`);
+});
+
+// C - create (POST)
+// R - read (GET)
+// U - update (PUT)
+// D - delete (DELETE)
+
+let potluck = [];
+
+// create
+server.post('/api/potluck', (req, res) => {
+  const potluckInfo = req.body;
+  potluckInfo.id = shortid.generate();
+  potluck.push(potluckInfo);
+
+  res.status(201).json(potluckInfo);
 });
