185f83f69b87f26cea6b65ec8d9d85e66749881c
aaron wesolek <maxwes13@gmail.com>
2021-01-07 22:57:16 -0600

    getting ready to seed

---
+++ auth/restricted-middware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const secrets = process.env.JWT_SECRET || "lambda";
+
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, secrets, (err, payload) => {
+      if (err) {
+        res.status(403).json({ message: "token not authorized" });
+      } else {
+        req.id = payload.id;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "no creds provided" });
+  }
+};
